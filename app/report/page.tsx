bca44b93aa6142993229d84d8987a659c24ade92
Sean N <xn@crc-dot1x-nat-10-239-78-183.bu.edu>
2025-09-29 19:39:27 -0400

    feat(ui): add student micro-credential report page

---
+++ app/report/page.tsx
@@ -0,0 +1,8 @@
+export default function ReportPage() {
+  return (
+    <main>
+      <h1>Student Micro-Credential Report</h1>
+      <p>Reporting dashboards will be implemented soon.</p>
+    </main>
+  );
+}
