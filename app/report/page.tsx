c95d9a2ca073e97ce54f29dc2e6ccc6c32814d66
Xiao Nie <nx2004@bu.edu>
2025-09-29 21:09:07 -0400

    Delete app/report directory
    Sorry, deleting accidentally pushed files, should've create a branch.

--- app/report/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ReportPage() {
-  return (
-    <main>
-      <h1>Student Micro-Credential Report</h1>
-      <p>Reporting dashboards will be implemented soon.</p>
-    </main>
-  );
-}
