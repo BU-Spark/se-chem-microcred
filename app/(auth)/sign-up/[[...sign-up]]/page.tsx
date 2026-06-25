1e2d30b417b107ef2f736059deb7d6588ceeede5
UgaTheDev <kush.zingade@gmail.com>
2026-06-25 16:27:39 +0800

    feat(onboarding): add account onboarding flow (info, demographics, avatar)

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function SignUpPage() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', padding: '2rem' }}>
-      <SignUp />
+      <SignUp forceRedirectUrl="/onboarding" signInUrl="/sign-in" />
     </div>
   );
 }
